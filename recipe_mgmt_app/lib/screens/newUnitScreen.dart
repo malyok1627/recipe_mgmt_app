b6a231bead9844cfc65416203259589a95eb2740
Yaroslav Smalko <yaroslavsmalko@gmail.com>
2019-02-06 11:59:34 +0100

    added validation of a textField. empty unit will not be saved

--- recipe_mgmt_app/lib/screens/newUnitScreen.dart
+++ recipe_mgmt_app/lib/screens/newUnitScreen.dart
@@ -19,6 +19,8 @@ class NewUnitScreenState extends State<NewUnitScreen> {
   DatabaseHelper databaseHelper = DatabaseHelper();
 
   MeasurementUnit unit;
+  // Used for text validation
+  final _formKey = GlobalKey<FormState>();
 
   // Value relevant for the Title
   TextEditingController nameController = TextEditingController();
@@ -33,48 +35,60 @@ class NewUnitScreenState extends State<NewUnitScreen> {
       appBar: AppBar(
         title: Text("Add new mesurement unit"),
       ),
-      body: Column(
-        children: <Widget>[
-          // Title of a measurement unit - TextField
-          // Unit name - TextField
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              controller: nameController,
-              style: titleText,
-              onChanged: (value) {
-                debugPrint("The value changed to: $value");
-                updateName();
-              },
-              decoration: InputDecoration(
-                labelText: "Unit",
-                labelStyle: titleText,
-                contentPadding:
-                    EdgeInsets.only(left: 20, bottom: 15.0, top: 15.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            // Title of a measurement unit - TextField
+            // Unit name - TextField
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: nameController,
+                style: titleText,
+                validator: (value) {
+                  if(value.isEmpty) {
+                    return "Please enter some text";
+                  } else {
+                    updateName();
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: "Unit",
+                  labelStyle: titleText,
+                  contentPadding:
+                      EdgeInsets.only(left: 20, bottom: 15.0, top: 15.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
             ),
-          ),
-          
-          // Save button
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              color: Theme.of(context).primaryColorDark,
-              textColor: Colors.black,
-              child: Text("Save", textScaleFactor: 1.5,),
-              onPressed: () {
-                setState(() {
-                  debugPrint("Save button pressed");
-                  _save();
-                });
-              },
-            ),
-          )
-        ],
-      )
+            
+            // Save button
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).primaryColorDark,
+                textColor: Colors.black,
+                child: Text("Save", textScaleFactor: 1.5,),
+                onPressed: () {
+                  setState(() {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, save the unit
+                      debugPrint("Save button pressed");
+                      _save();
+                    }
+                    
+                  });
+                },
+              ),
+            )
+          ],
+        )
+      )      
     );
   }
 
